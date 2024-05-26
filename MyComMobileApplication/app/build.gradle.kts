091f95c15218472e0281af0eb9c2d4d6950937e6
Kavinkumar Sainathan <kavincoolsout@gmail.com>
2024-05-25 17:34:04 -0700

    Mobile App
    Mobile App

---
+++ MyComMobileApplication/app/build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+}
+
+android {
+    namespace = "com.spyderkavin.mycommobileapplication"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.spyderkavin.mycommobileapplication"
+        minSdk = 28
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.12.0")
+    //WebView
+    implementation("androidx.webkit:webkit:1.8.0")
+
+}
---
+++ MyComMobileApplication/app/src/main/java/com/spyderkavin/mycommobileapplication/MainActivity.java
@@ -0,0 +1,69 @@
+package com.spyderkavin.mycommobileapplication;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    //WebView Code
+    private WebView mywebView;
+
+    //Post Notifications Code
+    private final ActivityResultLauncher<String> activityResultLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
+        @Override
+        public void onActivityResult(Boolean o) {
+            if (o) {
+                Toast.makeText(MainActivity.this, "Post notification permission granted!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    });
+    private static final int REQUEST_CODE = 100;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //findViewbyID
+        //mywebView = findViewById(R.id.webview);
+        setContentView(R.layout.activity_main);
+
+        ///Request Notification Permissions
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ActivityCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                activityResultLauncher.launch(Manifest.permission.POST_NOTIFICATIONS);
+            }
+        }
+
+/*
+        //Website Code Optimization
+        //Webview Code
+        mywebView.setWebViewClient(new WebViewClient());
+        WebSettings webSetting = mywebView.getSettings();
+        webSetting.setBuiltInZoomControls(true);
+        mywebView.setWebViewClient(new WebViewClient());
+        mywebView.loadUrl("nba.com");
+        WebSettings webSettings = mywebView.getSettings();
+        //webSettings.setJavaScriptEnabled(true);
+*/
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.loadUrl("https://github.com/amannindra/hackathon");
+
+    };
+}
